2e5e7d94d9952b9165d845b995b1185b76dc66a5
YassineHamdouni <yassinehamdouni023@gmail.com>
2025-08-24 16:59:17 +0100

    add home page

--- 01blog-frontend/src/app/app.routes.ts
+++ 01blog-frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import HomeComponent from './home/home';
+import LoginComponent from './login/login';
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        title: 'home'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'login'
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+    }
+];
